2956cbdbe2575e8ce7e2278fd6c2a15410ec7b2b
sibmaks <sibmaks@vk.com>
2024-09-13 22:42:58 +0300

    Add error localizations

--- src/main/java/com/github/simplemocks/localization/embedded/conf/LocalizationServiceEmbeddedConfig.java
+++ src/main/java/com/github/simplemocks/localization/embedded/conf/LocalizationServiceEmbeddedConfig.java
@@ -7,12 +7,23 @@ import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
+import com.github.simplemocks.error_service.mutable.api.source.ErrorLocalizationsJsonSource;
 import org.springframework.context.annotation.Bean;
 
 /**
  * @author sibmaks
  * @since 0.0.3
  */
+@ErrorLocalizationsJsonSource(
+        systemCode = "LOCALIZATION_SERVICE",
+        iso3Code = "eng",
+        path = "classpath:/embedded/content/localization/errors/eng.json"
+)
+@ErrorLocalizationsJsonSource(
+        systemCode = "LOCALIZATION_SERVICE",
+        iso3Code = "rus",
+        path = "classpath:/embedded/content/localization/errors/rus.json"
+)
 public class LocalizationServiceEmbeddedConfig {
 
     @Bean("localizationServiceObjectMapper")
